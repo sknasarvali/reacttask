4f81dd836f1716a55f089389dcb38deb460eb4c0
sknasarvali <sknasarvali007@gmail.com>
2020-06-23 12:56:19 +0530

    Updates

---
+++ precache-manifest.580611fbcfcd04c542e7a705ebb1937a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a16a5c1079b5cdc0badccba6cf3e5ea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c3176edcd3f0ad1de6cf",
+    "url": "/static/css/main.80f1e4b5.chunk.css"
+  },
+  {
+    "revision": "76ded9d29d085660c407",
+    "url": "/static/js/2.5f16d737.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.5f16d737.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3176edcd3f0ad1de6cf",
+    "url": "/static/js/main.8cb8e38a.chunk.js"
+  },
+  {
+    "revision": "a87051cab278ef87f339",
+    "url": "/static/js/runtime-main.eaa98dab.js"
+  },
+  {
+    "revision": "ecdb8c3044f0190a27c8f56f3acbbe80",
+    "url": "/static/media/man.ecdb8c30.svg"
+  }
+]);
